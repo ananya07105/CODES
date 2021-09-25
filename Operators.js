7681170d43f1c24ba9f9285a00a6ea440c0242f7
ANANYA SHARMA <66154902+ananya07105@users.noreply.github.com>
2021-09-25 22:45:31 +0530

    Create Operators.js

---
+++ Operators.js
@@ -0,0 +1,16 @@
+//Operators
+//Arithmetic Operators
+
+var a = 10;
+var b = 5;
+console.log(a + b);
+console.log(a * b);
+console.log(a - b);
+console.log(a / b);
+console.log(a % b);
+
+var a = 10;
+var b = 5;
+var c = a * b;
+c--;
+console.log(c);
