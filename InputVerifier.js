806ea728740fc85494e6c70e557f46387ad6f209
ANANYA SHARMA <66154902+ananya07105@users.noreply.github.com>
2021-09-25 22:43:15 +0530

    Create InputVerifier.js

---
+++ InputVerifier.js
@@ -0,0 +1,34 @@
+function readLine() {
+  return inputString[currentLine++];
+}
+function makeInputVerifier(minimum, maximum) {
+  function verify(input)
+{
+    
+  if(input<minimum)
+     return ("Input is less than minimum value");
+  if(input>=minimum&&input<=maximum)
+    return ("Input is in range");
+  if(input>maximum)
+    return ("Input is more than maximum value");
+  else
+    return;
+    
+}
+  
+  return verify;
+  //write your code here
+}
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const min = parseInt(readLine().trim());
+  const max = parseInt(readLine().trim());
+  const verify = makeInputVerifier(min, max);
+    
+  const input = parseInt(readLine().trim());
+  const result = verify(input);
+
+  ws.write(`${result}\n`);
+  ws.end();
+}
