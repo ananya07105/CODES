8e33f5b93ceb7fb35344910e4a719cf47aec52f7
ANANYA SHARMA <66154902+ananya07105@users.noreply.github.com>
2021-10-23 12:59:22 +0530

    Create Solution.cpp

---
+++ MCM(memoization)/Solution.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int dp[100][100];
+int MCM(int* p,int i,int j)
+{
+	if(i==j)
+	  return 0;
+	if(dp[i][j]!=-1)
+	{
+		return dp[i][j];
+	}
+	dp[i][j]=INT_MAX;
+	for(int k=i;k<j;k++)
+	{
+		dp[i][j]=min(dp[i][j],MCM(p,i,k)+MCM(p,k+1,j)+p[i-1]*p[k]*p[j]);
+	}
+	
+	return dp[i][j];
+}
+int MCMM(int *p,int n)
+{
+	int i=1,j=n-1;
+	return MCM(p,i,j);
+}
+int main()
+{
+	int arr[]={1,2,3,4};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	memset(dp,-1,sizeof(dp));
+	cout<<"Min number of multiplication is: "<<MCMM(arr,n);
+}
