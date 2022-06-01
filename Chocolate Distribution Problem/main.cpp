dd947296710d7e07d75a52ee9e494ee402307595
ANANYA SHARMA <66154902+ananya07105@users.noreply.github.com>
2022-06-01 17:32:26 +0530

    Create main.cpp

---
+++ Chocolate Distribution Problem/main.cpp
@@ -0,0 +1,13 @@
+int findMinDiff(int arr[],int n,int m)
+{
+  if(m==0||n==0)
+    return 0;
+  sort(arr,arr+n);
+  for(int i=0;i+m-1<n;i++)
+  {
+    int min_diff=INT_MAX;
+    if(diff<min_diff)
+      min_diff=diff;
+  }
+  return min_diff;
+}
